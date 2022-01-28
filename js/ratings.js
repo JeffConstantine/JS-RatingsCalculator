e70454422d4b33cc71972c8cdf4484e1126a4e9f
Jeff Constantine <conno80@gmail.com>
2022-01-28 13:53:16 +0800

    Update ratings.js
    updated forEach syntax

--- js/ratings.js
+++ js/ratings.js
@@ -1,10 +1,10 @@
-function collect_ratings(){
-    const ratings = {count: 0, sum: 0, average: 0};
+function collect_ratings() {
+  const ratings = { count: 0, sum: 0, average: 0 };
 
-    let rating = 0;
-    const elements = document.querySelectorAll('.rating');
+  let rating = 0;
+  const elements = document.querySelectorAll(".rating");
 
-   elements.forEach(element => {
-       
-   });
+  elements.forEach((element) => {
+    rating = parseInt(element.id.replace("star", ""));
+  });
 }
