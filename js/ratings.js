2771a9c3249540db6c0c645db1a0b3f6f53644dc
Jeff Constantine <conno80@gmail.com>
2022-02-03 09:23:42 +0800

    Update ratings.js
    added if statement to test ratings.count

--- js/ratings.js
+++ js/ratings.js
@@ -10,4 +10,8 @@ function collect_ratings() {
     ratings.count += parseInt(element.value);
     ratings.sum += parseInt(element.value) * rating;
   });
+
+  if (ratings.count !== 0) {
+    ratings.average = ratings.sum / ratings.count;
+    
 }
