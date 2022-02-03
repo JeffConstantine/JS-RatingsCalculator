8dcefe3ea3fed099c999dc8a0468dca8813df60c
Jeff Constantine <conno80@gmail.com>
2022-02-03 09:30:45 +0800

    Update ratings.js
    fixed error to ratings.count . sum and return

--- js/ratings.js
+++ js/ratings.js
@@ -11,7 +11,10 @@ function collect_ratings() {
     ratings.sum += parseInt(element.value) * rating;
   });
 
-  
- 
+  if (ratings.count !== 0) {
+    ratings.average = ratings.sum / ratings.count;
+  }
+   
+ return ratings;
     
-}
+};
