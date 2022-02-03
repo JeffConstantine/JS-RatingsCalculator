1fcd1df08340b15b99d31107740cb45d66fa5af7
Jeff Constantine <conno80@gmail.com>
2022-02-03 09:46:05 +0800

    Update ratings.js
    added last bit

--- js/ratings.js
+++ js/ratings.js
@@ -14,11 +14,11 @@ function collect_ratings() {
   if (ratings.count !== 0) {
     ratings.average = ratings.sum / ratings.count;
   }
-   
- return ratings;
-    
-};
 
-document.addEventListener('change', () => {
+  return ratings;
+}
 
+document.addEventListener("change", () => {
+  const ratings = collect_ratings();
+  document.querySelector('#average').value = ratings.average.toFixed(2);
 });
